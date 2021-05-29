f33d5235b91153bfd9a0f5d6dd5e8082406645b2
Tristan LaRoche <laroche11235@gmail.com>
2021-05-29 14:09:56 -0400

    import all the things I need

---
+++ src/components/MainContainer/MainContainer.js
@@ -0,0 +1,5 @@
+import React, { Component } from 'react'
+import SearchBar from '../SearchBar/Searchbar'
+import EmployeeTable from '../EmployeeTable/EmployeeTable'
+import API from '../../utils/API'
+import Wrapper from '../Wrapper/Wrapper';
