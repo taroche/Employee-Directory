1449b2f40e9dcadd4499c3545501e2b163b4c03f
Tristan LaRoche <laroche11235@gmail.com>
2021-05-29 14:10:31 -0400

    class to extend the component

--- src/components/MainContainer/MainContainer.js
+++ src/components/MainContainer/MainContainer.js
@@ -3,3 +3,21 @@ import SearchBar from '../SearchBar/Searchbar'
 import EmployeeTable from '../EmployeeTable/EmployeeTable'
 import API from '../../utils/API'
 import Wrapper from '../Wrapper/Wrapper';
+
+class MainContainer extends Component {
+    state = {
+        employees: [],
+        search: '',
+        filteredEmployees: [],
+        sorting: this.initialsorting,
+    };
+
+
+    get initialsorting() {
+        return {
+            name: "",
+            phone: "",
+            email: "",
+            dob: "",
+        };
+    }
