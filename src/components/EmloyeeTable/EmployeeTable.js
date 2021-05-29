b2ef3766bdfaac8383a3748a20db30f10b438357
Tristan LaRoche <laroche11235@gmail.com>
2021-05-29 14:04:19 -0400

    made the table body

--- src/components/EmloyeeTable/EmployeeTable.js
+++ src/components/EmloyeeTable/EmployeeTable.js
@@ -24,7 +24,30 @@ const EmployeeTable = (props) => {
                     </th>
                 </tr>
             </thead>
+            <tbody>
+                {props.state.filteredEmployees.map((employee) => {
+                    const { first, last } = employee.name;
+                    const fullName = `${first} ${last}`;
 
+                    // Format date
+                    const dob = props.formatDate(employee.dob.date);
+
+                    return (
+                        <tr>
+                            <td>
+                                <img className='img' src={employee.picture.thumbnail} alt={fullName} />
+                            </td>
+                            <td className='row-data'>{fullName}</td>
+                            <td className='row-data'>
+                                <a href={`tel:${employee.phone}`}>{employee.phone}</a></td>
+                            <td className='row-data email'>
+                                <a href={`mailto:${employee.email}`}>{employee.email}</a>
+                            </td>
+                            <td className='row-data'>{dob}</td>
+                        </tr>
+                    );
+                })}
+            </tbody>
         </table>
     )
 }
